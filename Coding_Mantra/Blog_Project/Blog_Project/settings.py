9460aff6137e70853c49b6d91ecd9fa5ee063cb4
Abhishek Bhardwaj <139381326+CodeEnthusiast96@users.noreply.github.com>
2023-12-07 22:36:00 +0530

    Update settings.py

--- Coding_Mantra/Blog_Project/Blog_Project/settings.py
+++ Coding_Mantra/Blog_Project/Blog_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7s$noz99q)aa2bu_$%em+lf-2j-y94gus@)%o6i^$ge4m8&)sb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
